c95f022ecca7dbce166c885ab38fb4b5e61bb47f
Baptiste Robert <bluebapt@gmail.com>
2021-12-06 09:42:20 +0100

    ca marche

--- index.php
+++ index.php
@@ -1,24 +1,13 @@
 <?php
-require_once './vendor/autoload.php';
-require_once './src/model/Liste.php';
-require_once './src/model/Item.php';
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \mywishlist\vue\VueCreateurListe as VueCreateurListe;
+require 'vendor/autoload.php';
+require 'src/vue/VueCreateurListe.php';
 
-use mywishlist\model\Item as Item;
-use mywishlist\model\Liste as Liste;
-use Illuminate\Database\Capsule\Manager as DB;
+$app = new \Slim\App;
+$app->get('/creer',function (Request $rq, Response $rs, $args):Response {
+    return VueCreateurListe::afficherFormulaire($rq,$rs,$args);
+});
 
-$db = new DB();
-$db->addConnection( ['driver'=>'mysql','host'=>'localhost','database'=>'td9',
-    'username'=>'wishmaster','password'=>'TropFort54','charset'=>'utf8','collation'=>'utf8_unicode_ci',
-    'prefix'=>''] );
-$db->setAsGlobal();
-$db->bootEloquent();
-
-// pour le select
-if(isset($_GET["id"])){
-    $id=$_GET["id"];
-    $res=Liste::select("titre")->where("no","=",$id)->get();
-    foreach ($res as $r){
-        echo $r->titre."<br>";
-    }
-}
+$app->run();
--- src/vue/VueCreateurListe.php
+++ src/vue/VueCreateurListe.php
@@ -1,8 +1,14 @@
 <?php
 
 namespace mywishlist\vue;
-
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+require_once 'vendor/autoload.php';
 class VueCreateurListe
 {
-
+    public static function afficherFormulaire(Request $rq, Response $rs, $args):Response{
+        $rs->getBody()->write("ici on va ajouter une liste de souhait<br>");
+        $rs->getBody()->write("les informations sont : titre, description et date d'expiration");
+        return $rs;
+    }
 }
