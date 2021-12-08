80f8811ab0b47f0862618860a2a88b27abf814e6
ThaLeeB57 <thomas.damas1@etu.univ-lorraine.fr>
2021-12-08 10:35:15 +0100

    mise en place VueAjoutItem

--- index.php
+++ index.php
@@ -2,12 +2,17 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use \mywishlist\vue\VueCreateurListe as VueCreateurListe;
+use \mywishlist\vue\VueAjoutItem as VueAjoutItem;
 require 'vendor/autoload.php';
 require 'src/vue/VueCreateurListe.php';
+require 'src/vue/VueAjoutItem.php';
 
 $app = new \Slim\App;
 $app->get('/creer/liste',function (Request $rq, Response $rs, $args):Response {
     return VueCreateurListe::afficherFormulaire($rq,$rs,$args);
+});
+$app->get('/ajout/item',function (Request $rq, Response $rs, $args):Response {
+    return VueAjoutItem::afficherFormulaire($rq, $rs, $args);
 });
 
 $app->run();
--- src/vue/VueAjoutItem.php
+++ src/vue/VueAjoutItem.php
@@ -2,7 +2,15 @@
 
 namespace mywishlist\vue;
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+require_once 'vendor/autoload.php';
+
 class VueAjoutItem
 {
-
+    public static function afficherFormulaire(Request $rq, Response $rs, $args):Response{
+        $rs->getBody()->write("ici on va ajouter un item dans la liste de souhait<br>");
+        $rs->getBody()->write("les informations sont : description, image et tarif");
+        return $rs;
+    }
 }
