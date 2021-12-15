83c42d8122991d7d0d444d8aa0d335426ad220aa
ThaLeeB57 <thomas.damas1@etu.univ-lorraine.fr>
2021-12-15 10:13:36 +0100

    debut de la page d'acceuil

--- src/vue/VueAcceuil.php
+++ src/vue/VueAcceuil.php
@@ -9,13 +9,38 @@ class VueAcceuil
 {
     public static function afficherFormulaire(Request $rq, Response $rs, $args):Response{
         $rs->getBody()->write(<<<END
-                    <body>
-                        <div class="container">
-                            <div class="liste">
-                                <p>hello</p>
+                    <!DOCTYPE html>
+                    <html>
+                        <head>
+                        <meta charset="utf-8" />
+                        <title>My wish list</title>
+                        <link rel="stylesheet" href="acceuil.css">
+                        </head>
+                        <body>
+                            <div class="page">
+                                <div class="title">
+                                    <h1>My Wish List</h1>
+                                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" id="rickroll"><img src="img/legend.png" alt="la legend" width="200em"></a>
+                                </div>
+                                <div class="menu">
+                                    <div class="liste">
+                                        <a href="http://127.0.0.1/mywishlist/creer/liste">Ajouter une liste</a>
+                                        <a href="">Afficher une liste</a>
+                                        <a href="">Modifier une liste</a>
+                                        <a href="">Partager une liste</a>
+                                    </div>
+                                    <div class="item">
+                                        <a href="http://127.0.0.1/mywishlist/ajout/item">Ajouter un item</a>
+                                        <a href="">Supprimer un item</a>
+                                        <a href="">Modifier un item</a>
+                                        <a href="">Ajouter une image</a>
+                                        <a href="">Modifier une image</a>
+                                        <a href="">Supprimer une image</a>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </body>
+                        </body>
+                    
                 END);
         return $rs;
     }
